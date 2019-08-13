1a8f515c84371b085e176f5297cd38f2f0f0bfb2
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-08-13 23:37:08 +0200

    react-the-complete-guide-redux: add Redux, create store, reducer and first actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux need to be define before React.DO.render
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; // We use provide to wrap our React APP to connect it with Redux
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
