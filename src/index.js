aaf4d59e5ec590ac77720ebf94ad27c9617992ce
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-10-03 22:26:50 +0200

    Create Redux Middleware

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Redux need to be define before React.DO.render
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'; // We use provide to wrap our React APP to connect it with Redux
 
 import counterReducer from './store/reducers/counter';
@@ -16,7 +16,20 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
-const store = createStore(rootReducer);
+
+// Create Middleware
+const logger = store => {
+    return next =>{
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
