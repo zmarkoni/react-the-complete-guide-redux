21532a0a61f005258b575ecef1f27b091ced7816
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-10-04 09:29:45 +0200

    Redux Writing cleaner Reducer with helper functions

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,19 +5,26 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    // filter return new array
+    const updatedArray = state.results.filter(el => el.id !== action.resultElID);
+    return updateObject(state, {results: updatedArray});
+};
+
+const storeResult = (state, action) => {
+    return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
+    // return {
+    //     ...state,
+    //     results: state.results.concat({id: new Date(), value: action.result})  // concat(add new item) return new array which is mandatory
+    // };
+};
+
 const reducer = (state = initialState, action) => {
     console.log('reducer: ', action);
     switch (action.type) {
-        case actionTypes.STORE_RESULT:
-            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
-            // return {
-            //     ...state,
-            //     results: state.results.concat({id: new Date(), value: action.result})  // concat(add new item) return new array which is mandatory
-            // };
-        case actionTypes.DELETE_RESULT:
-            // filter return new array
-            const updatedArray = state.results.filter(el => el.id !== action.resultElID);
-            return updateObject(state, {results: updatedArray});
+        // Writing cleaner Reducer with helper functions
+        case actionTypes.STORE_RESULT: return storeResult(state, action);
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action);
     }
     return state;
 };
